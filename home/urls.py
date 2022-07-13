146766d80dd1aa3d1e626e7d8374a1032b66dbb1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-13 22:11:27 +0430

    Defining about url

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from home.views import index_view
+from home.views import index_view, about_view
 
 # Defining app name
-app_name = "website"
+app_name = "home"
 
 urlpatterns = [
     path("", index_view, name="index"),
+    path("about", about_view, name="about"),
 ]
