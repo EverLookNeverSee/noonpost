3f2425a0535e2533f970cee12692164e45602d84
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-13 22:11:45 +0430

    Defining about view function

--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, "home/index.html")
+
+
+def about_view(request):
+    return render(request, "home/about.html")
