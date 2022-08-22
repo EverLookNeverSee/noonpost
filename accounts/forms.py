a5d3527542a4724fa9f7b411bab179a0e0da4165
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 11:24:36 +0430

    Implementing custom UserLoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Username or Email'}), label='Username or Email')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'placeholder': 'Password'}))
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
