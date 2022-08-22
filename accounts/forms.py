e4c43c39a6bc7e6c821d6df14939ddd0b7dd8270
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 11:28:30 +0430

    Implementing custom UserRegisterForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,3 +12,16 @@ class UserLoginForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                  'placeholder': 'Password'}))
     remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        }),
+        label='Email')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
