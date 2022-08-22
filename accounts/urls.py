8942b68c30219fb9a9807a3101396e6948d0502d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 13:08:48 +0430

    Defining accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,13 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from accounts.views import login_view, logout_view, signup_view, EmailValidationOnForgotPassword
+
+
+# Defining appname
+app_name = "accounts"
+
+urlpatterns = [
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("signup/", signup_view, name="signup"),
+]
