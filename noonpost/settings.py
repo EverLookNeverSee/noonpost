09132f2f90c876d89b471f86ad8415888b047ea3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 13:10:20 +0430

    Updating settings.py

--- noonpost/settings.py
+++ noonpost/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'home:index'
