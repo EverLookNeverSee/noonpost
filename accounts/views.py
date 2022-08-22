3bdafdcdcfa986b8694bb009528a8171918bd417
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 11:30:28 +0430

    Importing relevant packages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django import forms
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth import login, authenticate, logout, get_user_model
