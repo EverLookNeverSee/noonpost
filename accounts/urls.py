dc8fe977ab73bd25a1b7400ac9c4a5947986f2f9
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-26 14:20:06 +0430

    Defining password reset related urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,22 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("signup/", signup_view, name="signup"),
+
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset.html",
+        email_template_name="accounts/password_reset_email.html",
+        success_url=reverse_lazy("accounts:password_reset_done"),
+        form_class=EmailValidationOnForgotPassword
+    ), name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html",
+    ), name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html",
+        success_url=reverse_lazy("accounts:password_reset_complete")
+    ), name="password_reset_confirm"),
+    path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html",
+        title="Password reset completed."
+    ), name="password_reset_complete"),
 ]
