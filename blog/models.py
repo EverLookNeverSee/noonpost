85f6a7bf78b7b6697b66e050d666400f9327c273
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-28 22:21:54 +0430

    Implementing Comment model

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,22 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog:single", kwargs={"pid": self.id})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
