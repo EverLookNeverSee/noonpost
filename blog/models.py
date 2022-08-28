150f1d8704c93aa64ca1b82b80d418c0da348efe
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-28 22:20:48 +0430

    Implementing Post model

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,29 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
+    category = models.ManyToManyField(Category)
+    tags = TaggableManager()
+    count_views = models.PositiveIntegerField(default=0)
+    ok_to_publish = models.BooleanField(default=False)
+    login_required = models.BooleanField(default=False)
+    publish_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse("blog:single", kwargs={"pid": self.id})
