f68c9045957f6ac40195ff6bf9114a39ae77266b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-28 22:10:58 +0430

    Implementing category model

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
