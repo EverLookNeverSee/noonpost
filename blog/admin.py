092ba8fd3558a56e3f3a8829d0f6cc433adb6581
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-31 22:19:21 +0430

    Registering Post, Category and Comments models on admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from blog.models import Post, Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    date_hierarchy = "created_at"
+    empty_value_display = '-empty-'
+    list_display = ('title', 'author', 'count_views', 'ok_to_publish', 'created_at', 'published_date')
+    list_filter = ('author', 'ok_to_publish', 'created_date', 'publish_date')
+    search_fields = ('title', 'content')
+    summernote_fields = ('content',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    empty_value_display = '-empty-'
+    list_display = ('name', 'post', 'is_approved', 'created_at')
+    list_filter = ('is_approved', 'created_at', 'post')
+    search_fields = ('name', 'post')
