caed42e1c8c04d7ce831fafb9bb510ee1fc0b500
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-04 18:22:34 +0430

    Defining blog app urls

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,9 @@ app_name = "blog"
 
 urlpatterns = [
     path("", blog_index, name="index"),
-    path("single/", blog_single, name="single")
+    path("<int:pid>", blog_single, name="single"),
+    path("category/<str:cat_name>", blog_index, name="category"),
+    path("tag/<str:tag_name>", blog_index, name="tag"),
+    path("author/<str:author_username>", blog_index, name="author"),
+    path("search/", blog_search, name="search")
 ]
