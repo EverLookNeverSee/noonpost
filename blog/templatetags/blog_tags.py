840818aa7d6b1af567208222b334655d08591c99
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-04 12:38:25 +0430

    Implementing get_number_of_published_posts template_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,3 +4,12 @@ from blog.models import Post, Comment, Category
 # Defining registration object
 register = template.Library()
 
+
+
+@register.simple_tag(name="n_published_posts")
+def get_number_of_published_posts() -> int:
+    """
+    Get the number of published posts - ok_to_publish filed is True
+    :return: int, number of published posts
+    """
+    return Post.objects.filter(ok_to_publish=True).count()
