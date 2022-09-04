1efb0194e61b9200817cd4880dd921ee9cb45e02
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-04 18:32:47 +0430

    Refactoring blog_index view

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,24 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
-def blog_index(request):
-    return render(request, "blog/blog-home.html")
+def blog_index(request, **kwargs):
+    posts = Post.objects.filter(ok_to_publish=True, publish_date__lte=timezone.now()).order_by("-publish_date")
+    if kwargs.get("cat_name") is not None:
+        posts = posts.filter(category__name=kwargs["cat_name"], ok_to_publish=True)
+    if kwargs.get("author_username") is not None:
+        posts = posts.filter(author__username=kwargs["author_username"], ok_to_publish=True)
+    if kwargs.get("tag_name") is not None:
+        posts = posts.filter(tags__name__in=[kwargs["tag_name"]], ok_to_publish=True)
+    posts = Paginator(posts, 6)
+    try:
+        page_number = request.GET.get("page")
+        posts = posts.page(page_number)
+    except PageNotAnInteger:
+        posts = posts.page(1)
+    except EmptyPage:
+        posts = posts.page(1)
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
 
 
 def blog_single(request):
