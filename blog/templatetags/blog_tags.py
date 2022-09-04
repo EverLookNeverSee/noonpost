b1e276e3fa95d7695050c14d74369902100fac51
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-04 12:42:49 +0430

    Implementing get_published_posts template_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post, Comment, Category
+from typing import List
 
 # Defining registration object
 register = template.Library()
@@ -13,3 +14,12 @@ def get_number_of_published_posts() -> int:
     :return: int, number of published posts
     """
     return Post.objects.filter(ok_to_publish=True).count()
+
+
+@register.simple_tag(name="published_posts")
+def get_published_posts() -> List[Post]:
+    """
+    Get the list of published posts - ok_to_publish filed is True
+    :return: List, published posts
+    """
+    return Post.objects.filter(ok_to_publish=True)
