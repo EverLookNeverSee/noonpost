c128786c1e7f316fb52a22be0f116f156d34615d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-10 21:44:06 +0430

    Importing relavant packages

--- noonpost/urls.py
+++ noonpost/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from home.sitemaps import StaticViewSitemap
 
 
 urlpatterns = [
