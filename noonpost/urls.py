ef074be2d18f56ec3f3a837ce2d4ab82e13bd4b3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-10 21:45:37 +0430

    Adding sitemap url

--- noonpost/urls.py
+++ noonpost/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path("summernote/", include('django_summernote.urls'))
+    path("summernote/", include('django_summernote.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
 
 
