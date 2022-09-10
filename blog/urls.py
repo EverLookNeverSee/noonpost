a5498d1eeb9afa63b5b665be5644ac7b7c827c2d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-10 23:54:28 +0430

    Defining rss feed url path

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog.views import *
+from .feeds import LatestPostsRSSFeed
 
 
 app_name = "blog"
@@ -11,5 +12,6 @@ urlpatterns = [
     path("category/<str:cat_name>", blog_index, name="category"),
     path("tag/<str:tag_name>", blog_index, name="tag"),
     path("author/<str:author_username>", blog_index, name="author"),
-    path("search/", blog_search, name="search")
+    path("search/", blog_search, name="search"),
+    path("rss/feed", LatestPostsRSSFeed())
 ]
