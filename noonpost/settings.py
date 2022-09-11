4a0b391784ca6b4df4e29a39680edaa34d7399bb
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-11 22:07:47 +0430

    removing unused secret key from settings.py

--- noonpost/settings.py
+++ noonpost/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 
 # Application definition
